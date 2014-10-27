95feafc9de9ad5fc395337e07a503346c2d2b19a
Robert Agthe <robert@scriptshit.de>
2014-10-27 04:54:56 +0100

    remove www/ from install path
    added bower install

--- public/uberspace_install.sh
+++ public/uberspace_install.sh
@@ -3,31 +3,35 @@ cd ~
 git clone https://github.com/polarity/MyFeed.git
 
 # install dependcies
-cd ~/MyFeed/public/www/
+cd ~/MyFeed/public/
 npm install
+locally
+# install bower locally
+npm install -g --prefix=$HOME bower
+bower install
 
 # create environment
-touch /MyFeed/public/www/.env
+touch /MyFeed/public/.env
 
 echo "please enter the serverport"
 read serverport
-echo SERVERPORT = $serverport > ~/MyFeed/public/www/.env
+echo SERVERPORT = $serverport > ~/MyFeed/public/.env
 
 echo "Whats your domain you running MyFeed under? (example: http://robert-agthe.de) without trailling slash"
 read domain
-echo DOMAIN_URL="$domain" >> ~/MyFeed/public/www/.env
+echo DOMAIN_URL="$domain" >> ~/MyFeed/public/.env
 
 echo "Whats your EMail? Used for Gravatars and attached to every post Metadata! Be careful here. (example: mymail@mydomain.com)"
 read email
-echo USER_EMAIL="$email" >> ~/MyFeed/public/www/.env
+echo USER_EMAIL="$email" >> ~/MyFeed/public/.env
 
 echo "Specify your passwort for authorizing to post content!"
 read password
-echo USER_PASSWORD="$password" >> ~/MyFeed/public/www/.env
+echo USER_PASSWORD="$password" >> ~/MyFeed/public/.env
 
 echo "You´re Name. Used for the header and attached to all postings. (example: Robert Agthe)"
 read username
-echo USER_NAME="$username" >> ~/MyFeed/public/www/.env
+echo USER_NAME="$username" >> ~/MyFeed/public/.env
 
 # create redirect
 cd ~/html
@@ -37,4 +41,4 @@ echo "RewriteRule ^(.*) http://localhost:$serverport/\$1 [P]" >> ~/html/.htacces
 
 # create Service
 test -d ~/service || uberspace-setup-svscan
-uberspace-setup-service myfeed node ~/MyFeed/public/www/index.js
+uberspace-setup-service myfeed node ~/MyFeed/public/index.js
